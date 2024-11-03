fe4d20ed01e97eba60d604e1d5c5e8a499aee5bb
sumayaakter533 <sumaya55230@gmail.com>
2024-11-03 12:09:19 +0600

    react loader added

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis } from 'recharts';
+import { Audio } from 'react-loader-spinner';
 
 const Phones = () => {
 	const [phones, setPhones] = useState([]);
+	const [loading, setLoading] = useState(true);
+
 	useEffect(() => {
 		// fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
 		// 	.then((res) => res.json())
@@ -21,10 +24,24 @@ const Phones = () => {
 					return phoneObj;
 				});
 				setPhones(phoneFakeData);
+				setLoading(false);
 			});
 	}, []);
 	return (
 		<div className="text-center py-10">
+			{loading && (
+				<div className="h-screen flex justify-center">
+					<Audio
+						height={`80`}
+						width={`80`}
+						radius="9"
+						color="green"
+						ariaLabel="three-dots-loading"
+						wrapperClass
+						wrapperStyle
+					></Audio>
+				</div>
+			)}
 			<h3 className="text-4xl mb-8">Phones: {phones.length}</h3>
 			<BarChart width={1200} height={400} data={phones} className="m-auto">
 				<XAxis dataKey={`name`}></XAxis>
